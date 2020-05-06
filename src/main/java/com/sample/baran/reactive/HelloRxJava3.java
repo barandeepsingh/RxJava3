a1a40477902c5b8b4e5bf3796c7988e5738bced0
Barandeep Singh <barandeepsingh@gmail.com>
2020-05-06 22:30:26 +0530

    Initial commit

---
+++ src/main/java/com/sample/baran/reactive/HelloRxJava3.java
@@ -0,0 +1,17 @@
+package com.sample.baran.reactive;
+import io.reactivex.rxjava3.core.Observable;
+
+public class HelloRxJava3 {
+    public static void main(String[] args) {
+        Observable<String> source = Observable.create(
+                e -> {
+                    e.onNext("Hello");
+                    e.onNext(" RxJava3");
+                }
+
+        );
+        source.subscribe(e -> System.out.println("Observer 1: "+e));
+        source.subscribe(e -> System.out.println("Observer 2: "+e));
+    }
+
+}
